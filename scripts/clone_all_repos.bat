f432251fb1fc9fa0c22bbff7c3a1367ae2fc0c1f
Jordan Doose <111624006+doosedev@users.noreply.github.com>
2022-09-23 20:16:22 -0700

    Update repo URLs

--- scripts/clone_all_repos.bat
+++ scripts/clone_all_repos.bat
@@ -3,12 +3,12 @@ echo Cloning all 2022-2023 repositories...
 echo.
 
 cd ../..
-git clone git@github.com:RoboSubCSULA/seaking_frame.git
-git clone git@github.com:RoboSubCSULA/seaking_actuators.git
-git clone git@github.com:RoboSubCSULA/seaking_electrical.git
-git clone git@github.com:RoboSubCSULA/seaking_controls.git
-git clone git@github.com:RoboSubCSULA/seaking_autonomy.git
-git clone git@github.com:RoboSubCSULA/seaking_vision.git
+git clone git@github.com:RoboSubCSULA/wailord_frame.git
+git clone git@github.com:RoboSubCSULA/wailord_actuators.git
+git clone git@github.com:RoboSubCSULA/wailord_electrical.git
+git clone git@github.com:RoboSubCSULA/wailord_controls.git
+git clone git@github.com:RoboSubCSULA/wailord_autonomy.git
+git clone git@github.com:RoboSubCSULA/wailord_vision.git
 
 echo.
 echo Done!
